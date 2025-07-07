4dfadcf6c83dc91fff571141b1de3fffe3f31754
Abhishek Jaiswal <100372101+Codewiththinkabhi@users.noreply.github.com>
2025-07-07 22:46:57 +0530

    Create current_year-shortcode.php
    Shortcode for current year in footer

---
+++ current_year-shortcode.php
@@ -0,0 +1,18 @@
+<?php
+/*
+*Plugin Name: Current year Shortcode Plugin 
+*Description: This plugin is used to create shortcode 
+*Version:1.0
+*Author: Abhishek Jaiswal 
+*/
+
+
+//Shortcode for current year Automatically keeps the year updated without editing your footer manually
+
+add_shortcode('year','current_year_shortcode');
+
+function current_year_shortcode (){
+return date ('Y');
+}
+
+?>
