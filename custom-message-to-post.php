6bf31558f6705c49a5e88d9c808acdd4708beb2d
Abhishek Jaiswal <100372101+Codewiththinkabhi@users.noreply.github.com>
2025-07-12 23:24:20 +0530

    Update custom-message-to-post.php
    updated

--- custom-message-to-post.php
+++ custom-message-to-post.php
@@ -6,10 +6,15 @@
 *Author Name: Abhishek Jaiswal
 */
 
+
 function add_custom_messgae_to_posts ($content) {
+if (is_single()){
+$content.='<p><em>Thankyou for  reading this post !</em></p>';
+}
+return $content;
+}
 
-
-
+add_filter('the_content','add_custom_message_to_post');
 
 
 ?>
