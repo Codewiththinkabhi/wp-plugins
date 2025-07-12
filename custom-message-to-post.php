fea091a836a9a86704a3746621c2ecba27658f70
Abhishek Jaiswal <100372101+Codewiththinkabhi@users.noreply.github.com>
2025-07-12 23:10:28 +0530

    Create custom-message-to-post.php
     plugin to append text inn the blog post  (Add a message at the end of every blog post)

---
+++ custom-message-to-post.php
@@ -0,0 +1,15 @@
+<?php
+**/
+*Plugin name: my firat plugin 
+*Description: A sample wordpress plugin example.
+*version : 1.0
+*Author Name: Abhishek Jaiswal
+*/
+
+function add_custom_messgae_to_posts ($content) {
+
+
+
+
+
+?>
